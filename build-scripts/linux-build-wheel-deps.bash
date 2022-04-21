d740f891c56559cea6a2e69b4439099e286f65e9
Anil Kumar Reddy <92432529+anilreddypuresoftware@users.noreply.github.com>
2022-04-21 13:01:13 +0530

    Update linux-build-wheel-deps.bash

--- build-scripts/linux-build-wheel-deps.bash
+++ build-scripts/linux-build-wheel-deps.bash
@@ -15,7 +15,11 @@ fi
 
 if [ ! -f /usr/local/lib/libqpdf.a ]; then
     pushd qpdf
-    ./configure --disable-oss-fuzz && make -j install-libs
+    if [[ $(uname -p) == 'aarch64' ]]; then
+        ./configure --disable-oss-fuzz && make install-libs
+    else
+        ./configure --disable-oss-fuzz && make -j install-libs
+    fi
     find /usr/local/lib -name 'libqpdf.so*' -type f -exec strip --strip-debug {} \+
     popd
 fi
